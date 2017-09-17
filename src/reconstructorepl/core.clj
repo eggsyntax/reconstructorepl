7bdd2ec0f39531be2c8a4bc3be7f0f664bbfb897
Egg Syntax <egg@novonon.com>
2017-09-17 15:10:35 -0400

    Add `form` fn; refactor

--- src/reconstructorepl/core.clj
+++ src/reconstructorepl/core.clj
@@ -19,7 +19,19 @@
   registry (alhough there must be other things the history is good for)
 * Note that we can cache the histories.
 * TODO don't add exception-throwing expressions to history
+* Known failure mode: if you def a as 1, then def b as (inc a), then redefine a
+  as 100, and then (build-defs) and replay them in a new namespace, b will
+  reflect the newest value of a, and hence differ from its previous value.
+  This has the potential to suprprise, but I believe it matches actual repl use
+  more closely; when you redefine a var in the repl, it's usually because you're
+  changing it from an incorrect value to a correct value, and so other vars
+  based on it should change as well. At some point I may consider making it
+  possible to specify which behavior you prefer.
+
 "
+
+;; TODO set a dynamic var to control whether forms are pulled from
+;; metadata or from a registry
 
 ;; Terms: by 'defr' I mean 'def or defn'
 ;;        by 'hx' I mean 'history'
@@ -43,6 +55,13 @@
 
 (defonce user-history (atom []))
 (defonce defr-history (atom {}))
+
+(defn form
+  "Given a symbol (typically passed quoted), returns the form which
+  defined it, if it can be found."
+  [sym]
+  (or (:form (meta (resolve sym)))
+      (clojure.repl/source-fn sym)))
 
 (defn add-to-history
   "Does a 2-level merge -- adds the current def/defn to the map of them stored
@@ -77,7 +96,7 @@
               (main/skip-if-eol *in*)
               input)))))
 
-(defn- prompt []
+(defn prompt []
   (print "***> "))
 
 (defn expr-type
@@ -169,24 +188,30 @@
                  (cond (and (symbol? el) (not (is-param? el))) el
                        (sequential? el) (needed-defrs el params)))))))))
 
-(defn build-code-for
-  "Build a list containing current and all children where children are the defrs
-  that must be performed before current."
-  [sym]
-  (if-let [sym-defr (@defr-history sym)]
-    ;; Locally defined:
+(declare build-code-for)
+
+(defn local-definition [sym]
+  (when-let [sym-defr (@defr-history sym)]
     (conj
      (apply concat ; Was removing nils from this, but maybe I don't need to?
             (for [subsym (needed-defrs sym-defr)]
               (build-code-for subsym)))
-     sym-defr)
-    ;; Not locally defined:
-    (if (or (resolve sym) (special-symbol? sym))
-      ;; if we can resolve it or it's a special form, it's defined elsewhere and
-      ;; we can just ignore it.
-      nil
-      ;; Otherwise it's undefined.
-      (throw (Exception. (str sym " is not defined."))))))
+     sym-defr)))
+
+(defn build-code-for
+    "Build a list containing current and all children where children are the defrs
+  that must be performed before current."
+    [sym]
+    (if-let [sym-defr (@defr-history sym)]
+      ;; Locally defined:
+      (local-definition sym)
+      ;; Not locally defined:
+      (if (or (resolve sym) (special-symbol? sym))
+        ;; if we can resolve it or it's a special form, it's defined elsewhere and
+        ;; we can just ignore it.
+        nil
+        ;; Otherwise it's undefined.
+        (throw (Exception. (str sym " is not defined."))))))
 
 (defn build-defs
   "Return a sequence of statements which, run in order, will let you define
